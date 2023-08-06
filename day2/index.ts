2cd2f6f16772d7738c314b5bf32ad6dc3dd1fda0
nirmal kumar <nirmalkumargurajada@gmail.com>
2023-08-06 21:27:53 +0530

    day2

---
+++ day2/index.ts
@@ -0,0 +1,3 @@
+let user = { name: "Nirmal", age: 24 };
+console.log(user.name)
+console.log(user.email) // This line of code is giving squiggly line as there is no email in user object this is the benefit of typescript.
