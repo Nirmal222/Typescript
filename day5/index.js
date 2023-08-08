3b42dd9f2de74c64d81bf4f002cd2056cc9eb296
nirmal kumar <nirmalkumargurajada@gmail.com>
2023-08-08 22:53:58 +0530

    Objects

---
+++ day5/index.js
@@ -0,0 +1,12 @@
+"use strict";
+//Objects
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "Nirmal",
+    email: "nirmal@gmail.com",
+    isActive: true
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createUser({});
