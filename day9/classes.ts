785f9896fdd6ade1df009cf494d4524ad513c926
nirmal kumar <nirmalkumargurajada@gmail.com>
2023-08-12 23:30:10 +0530

    Access Modifiers

--- day9/classes.ts
+++ day9/classes.ts
@@ -1,5 +1,6 @@
 class User {
-    
+    // private _courseCount = 1
+    protected _courseCount = 1
     readonly city: string = "Kakinada"
     // private city: string = "Kakinada" The (private/#) modifier does not allow the city to be accessible outside the class definition
     // readonly city: string = "Kakinada"
@@ -10,5 +11,32 @@ class User {
         private userId: string
         ){ // This is one way to define class properties without using this. keyword
     }
+
+    // getter method is designed to give access to private properties
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+    // setter 
+    set courseCount(courseNumber){ //whenever using setter always ther should be no return value
+        if(courseNumber<=1) throw new Error("Course count should be more than 1");
+        this._courseCount = courseNumber;
+    }
+    // privateMethods
+    private deleteToken(){
+        console.log("Token deleted");
+    }
 }
 
+// Public, Protected and Private are called access modifiers
+
+// Inhertitance 
+class SubUser extends User{
+    isFamily: boolean = true;
+    changeCourseCount(){
+        this._courseCount = 4 //As the _courseCount is a private variable we cannot get that variable inside inherited class to solve this we can use protected to make it accesible in inherited classes
+    }
+}
