1ad80774be9195f3c79c69cb9c31f6a0e8cfa071
nirmal kumar <nirmalkumargurajada@gmail.com>
2023-08-16 22:50:44 +0530

    In Operator

---
+++ day13/in.ts
@@ -0,0 +1,15 @@
+interface User {
+    name: string,
+    email: string
+}
+
+interface Admin {
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
+
+function isAdminAccount(account: User | Admin){
+    if("isAdmin" in account) return account.isAdmin;
+    return account.email
+}
